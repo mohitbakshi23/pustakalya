a91f1b403dfa828a713ee4ac81ceb93c6a567fd6
mohitbakshi23 <22557152+mohitbakshi23@users.noreply.github.com>
2021-07-18 17:33:19 +0530

    Update editrequest1.php

--- editrequest1.php
+++ editrequest1.php
@@ -96,14 +96,14 @@ echo '<center><span style="color:white;text-align:center;"><h2>Request Edited.<h
   </li>
   <li>
     Address:
-    VIT Vellore,
-    Katpadi Road,
-    Vellore,
-    Tamil Nadu 632014
+     Govt. College for Women,
+    Parade Ground,
+    Jammu,
+    Jammu&kashmir 180001.
   </li>
   <li>
     Contact Us:
-    Email:vitbarnbooks@gmail.com
+    Email:buybooks@gmail.com
     Mobile No: +918178197468
   </li>
   </ul>
